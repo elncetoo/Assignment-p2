dde05a7ff02151e7302ac606e19599aa5f79603d
elle <svilena_koleva@yahoo.com>
2016-09-28 22:58:31 +0200

    dehashing psswd

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -5,13 +5,25 @@ include '../dbh.php';
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+$sql = "SELECT * FROM user WHERE uid='$uid'";
 $result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$hash_pwd = $row['pwd'];
+$hash = password_verify($pwd, $hash_pwd);
 
-if (!$row = mysqli_fetch_assoc($result)) {
-	echo "Your username or password is incorrect!";
+if ($hash == 0){
+	header("Location: ../index.php?error=empty");
+	exit();
 } else {
-	$_SESSION['id'] = $row['id'];
-}
 
-header("Location: ../index.php");
+	$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$hash_pwd'";
+	$result = mysqli_query($conn, $sql);
+	
+	if (!$row = mysqli_fetch_assoc($result)) {
+		echo "Your username or password is incorrect!";
+	} else {
+		$_SESSION['id'] = $row['id'];
+	}
+	
+	header("Location: ../index.php");
+}
