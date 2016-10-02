a6a82668c5f1f0e2b7b601f711bad31a1528b177
elle <svilena_koleva@yahoo.com>
2016-10-02 15:50:00 +0200

    implementing
    implementing the log in page to the multiple web page from assignment
    part 1 + addinf the secret page

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
-<?php
-	include 'header.php';
-?>
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="css/styles.css">
+<meta charset="utf-8">
+<title>PHP test page</title>
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
 
-    <?php
-		if(isset($_SESSION['id'])) {
-			echo "<h1>Hi there user! </h1>";
-		} else {
-			echo "You are not logged in!";
-		}
-	?>
-        
-    
-    </br></br></br>
-      
-</body>
-</html>
+<body>
+<?php include 'menu.php';?>
+<br>
+<h1 style="text-align:center; padding: 5px;">Target Branding by Allan Peters</h1>
+
+
+  <article class="article">
+  <img src="img/buble.png" alt="buble" style="width:100%;height:100%;">
+  </article>
+
+
+<?php require 'footer.php';?>
+
